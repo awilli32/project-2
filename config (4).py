3347cf181497e96b07812252478be99ae47a8d4a
awilli32 <73446610+awilli32@users.noreply.github.com>
2021-02-18 12:01:32 -0500

    Add files via upload

---
+++ config (4).py
@@ -0,0 +1,13 @@
+# Local Postgres DB Info
+local_db_user = 'postgres'
+local_db_pwd = 'postgres'
+local_db_endpoint = 'localhost'
+local_db_port = '5432'
+local_db_name = 'project2_db'
+
+# Cloud/AWS MySQL DB Info
+remote_db_endpoint = 'codingbootcamp.c3au28hegzm9.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'vthokies67'
+remote_db_name = 'fantasy_project'
